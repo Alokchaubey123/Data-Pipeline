9829f9c6407ab39536069c8a75b295b59b25a664
Alokchaubey123 <alokchaubey116@gmail.com>
2026-08-05 18:30:30 +0530

    fetched and stored weather data in json

--- fetch_w_data.py
+++ fetch_w_data.py
@@ -1,11 +1,21 @@
 import requests
 import os
 
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
 API_KEY = os.environ.get("OPENWEATHER_API_KEY", "")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-
+CITIES = [
+    "London",
+    "Tokyo",
+    "Delhi",
+    "Paris",
+    "Kyoto",
+    "Kolkata"
+]
 
 def fetch_weather_for_city(city):
     #fetching city data
@@ -28,3 +38,63 @@ def fetch_weather_for_city(city):
     except requests.exceptions.RequestException:
         print(f"Request failed for {city}")
         return None
+
+def main():
+
+    print("Fetching weather data searching started")
+
+
+
+    if API_KEY:
+        print("Fetching using OpenWeather API\n")
+
+    else:
+
+        print("API key not found.\n")
+
+
+
+    weather_data = []
+
+
+
+    for city in CITIES:
+
+        print(f"Getting data for {city}")
+
+
+
+        if API_KEY:
+
+            city_data = fetch_weather_for_city(city)
+
+        else:
+
+            print("city_data not found.\n")
+            break
+
+
+
+        if city_data is not None:
+
+            weather_data.append(city_data)
+
+
+    os.makedirs("data", exist_ok=True)
+
+    output_file = "data/raw_weather.json"
+
+    with open(output_file, "w") as file:
+
+        json.dump(weather_data, file, indent=4)
+
+
+
+    print(f"\nData Saved in raw_weather.json")
+
+
+
+if __name__ == "__main__":
+
+    main()
+
