7b578342cc2810a515e054d86724157628d94e0a
Alokchaubey123 <alokchaubey116@gmail.com>
2026-08-05 18:50:17 +0530

    Created dataframe raw data to a row coloum format

--- clean_w_data.py
+++ clean_w_data.py
@@ -8,7 +8,26 @@ def main():
         weather_data = json.load(file)
 
     print(f"Loaded {len(weather_data)} records")
-    print(weather_data)
+
+    data = []
+    for item in weather_data:
+
+        data.append({
+            "city": item["name"],
+            "country": item["sys"]["country"],
+            "temperature": item["main"]["temp"],
+            "feels_like": item["main"]["feels_like"],
+            "temp_min": item["main"]["temp_min"],
+            "temp_max": item["main"]["temp_max"],
+            "humidity": item["main"]["humidity"],
+            "pressure": item["main"]["pressure"],
+            "wind_speed": item["wind"]["speed"],
+            "weather": item["weather"][0]["main"],
+            "description": item["weather"][0]["description"],
+            "timestamp": item["dt"]
+        })#all this data is present in .json file, some points are extracted 
+    df = pd.DataFrame(data)
+    print(df)
 
 if __name__ == "__main__":
     main()
